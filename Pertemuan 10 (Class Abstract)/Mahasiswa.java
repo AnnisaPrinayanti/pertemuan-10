a58b48cb9d72c8b02fb2cb2911068d803b084ef2
AnnisaPrinayanti <32536255+AnnisaPrinayanti@users.noreply.github.com>
2017-12-22 18:46:54 +0700

    Add files via upload

---
+++ Pertemuan 10 (Class Abstract)/Mahasiswa.java
@@ -0,0 +1,10 @@
+public abstract class Mahasiswa { //abstract class
+	protected String nama = "Rendy";
+	protected String nim = "12121212";
+
+	protected String getNama () {
+	return nama;
+	}
+
+	public abstract void printNama(); //abstract method
+}
---
+++ Pertemuan 10 (Class Abstract)/Rendy.java
@@ -0,0 +1,15 @@
+public class Rendy extends Mahasiswa {
+	protected String getNama() {
+	return super.getNama();
+	}
+	
+	public void printNama() {
+		System.out.println(getNama());
+	}
+
+	public static void main (String[] args) {
+		Rendy rendy = new Rendy();
+		rendy.printNama();
+
+	}
+}
